d3592c182e586fc4f0fc3519f0bd5223acab6b77
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-09-10 14:15:41 +0530

    intial commit

---
+++ src/Test.js
@@ -0,0 +1,15 @@
+// function Greeting() {
+//   return (
+//     <div>
+//       <h2>hello world</h2>
+//     </div>
+//   );
+// }
+
+// const Greeting = () => {
+//   return React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h2', {}, 'hello world')
+//   );
+// };
